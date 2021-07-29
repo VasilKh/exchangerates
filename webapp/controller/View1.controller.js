8afb6038ff8721f3f11fca20f31d0a8ae6786027
VasilKh <vasil.khiliuk@gmail.com>
2021-07-29 12:19:09 +0300

    add valueAxis

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,73 +7,91 @@ sap.ui.define([
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
 
-function (Controller, formatter, Filter) {
-	"use strict";
+	function (Controller, formatter, Filter) {
+		"use strict";
 
-	var currency = '';
-	var dateFrom = new Date();
-	var dateTo = new Date();
-	var oModel;
-	
+		var currency = '';
+		var dateFrom = new Date();
+		var dateTo = new Date();
+		var oModel;
 
-	return Controller.extend("exchangerates.controller.View1", {		
-		formatter: formatter,
 
-		onInit: function(evt) {
-			var sUrl = "/sap/opu/odata/sap/ZVK_I_ERATE_UI/";
-			oModel = new sap.ui.model.odata.ODataModel(sUrl, {
-				useBatch: false
-			});
+		return Controller.extend("exchangerates.controller.View1", {
+			formatter: formatter,
 
-			var oFilter = new Filter("CurrencyKey",sap.ui.model.FilterOperator.EQ,currency);
+			onInit: function (evt) {
+				var sUrl = "/sap/opu/odata/sap/ZVK_I_ERATE_UI/";
+				oModel = new sap.ui.model.odata.ODataModel(sUrl, {
+					useBatch: false
+				});
 
-			this.getView().setModel(oModel);
+				var oFilter = new Filter("CurrencyKey", sap.ui.model.FilterOperator.EQ, currency);
 
-			this.getView().byId("idVizFrame").getDataset().getBinding("data").filter([oFilter]);
-	
-		},
+				this.getView().setModel(oModel);
 
-		onDateChoose(oEvent){
-			dateFrom = oEvent.getParameters().from;
-			dateFrom.setDate(dateFrom.getDate() + 1);
-			dateTo = oEvent.getParameters().to;
-		},
+				this.getView().byId("idVizFrame").getDataset().getBinding("data").filter([oFilter]);
+				this.currency();
+			},
 
-		onCurrencyChoose(oEvent){
-			currency = oEvent.getParameters().value;
-		},
+			currency: function () {
+				var uri = 'https://www.nbrb.by/API/';
+				var that = this;
+				$.getJSON(uri + 'ExRates/Currencies/')
+					.done(function (data) {
 
-		onSyncronize: function(oEvent){
-			oModel.callFunction('synchronization', { urlParameters:{
-				Currencykey : currency},
-				method:'POST'
+						var oModel1 = new sap.ui.model.json.JSONModel(data);
+						that.getView().setModel(oModel1,"asdf");
+
+					}).error(function (err) {
+						$('#btn').removeAttr("disabled");
+						alert('ошибка');
+					});
+			},
+
+			onDateChoose(oEvent) {
+				dateFrom = oEvent.getParameters().from;
+				dateFrom.setDate(dateFrom.getDate() + 1);
+				dateTo = oEvent.getParameters().to;
+			},
+
+			onCurrencyChoose(oEvent) {
+				currency = oEvent.getParameters().value;
+			},
+
+			onSyncronize: function (oEvent) {
+				oModel.callFunction('synchronization', {
+					urlParameters: {
+						Currencykey: currency
+					},
+					method: 'POST'
 				})
-		},
+				
+			},
 
-		onSearch: function (oEvent){
+			onSearch: function (oEvent) {
 
-		/* 	oModel.callFunction('synchronization', { urlParameters:{
-				Currencykey : currency},
-				method:'POST'
+				/* 	oModel.callFunction('synchronization', { urlParameters:{
+						Currencykey : currency},
+						method:'POST'
+						}); */
+				/* var oFilter = [
+					new Filter("CurrencyKey", "EQ", currency),
+					new Filter("ERDate", "BT", dateFrom, dateTo),
+				];
+				oModel.read("/ZVK_CDS_ERATES", {
+					filters : oFilter,
 				}); */
-			/* var oFilter = [
-				new Filter("CurrencyKey", "EQ", currency),
-				new Filter("ERDate", "BT", dateFrom, dateTo),
-			];
-			oModel.read("/ZVK_CDS_ERATES", {
-				filters : oFilter,
-			}); */
 
-			var oFilter = new Filter("CurrencyKey",sap.ui.model.FilterOperator.EQ,currency);
-			var oFilter2 = new Filter("ERDate",sap.ui.model.FilterOperator.BT, dateFrom, dateTo);
+				var oFilter = new Filter("CurrencyKey", sap.ui.model.FilterOperator.EQ, currency);
+				var oFilter2 = new Filter("ERDate", sap.ui.model.FilterOperator.BT, dateFrom, dateTo);
 
-			var oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZVK_I_ERATE_UI");
-			this.getView().setModel(oModel);
+				var oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZVK_I_ERATE_UI");
+				this.getView().setModel(oModel);
 
-			this.getView().byId("idVizFrame").getDataset().getBinding("data").filter([oFilter, oFilter2]);
-		},
+				this.getView().byId("idVizFrame").getDataset().getBinding("data").filter([oFilter, oFilter2]);
+			},
 
 
 
+		});
 	});
-});
